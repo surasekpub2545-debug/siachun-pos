02d67b72457e50825a8da4d07e769f41e42c3143
surasekpub2545-debug <surasekpub2545@users.noreply.github.com>
2026-05-13 17:08:35 +0700

    Replace app icon with custom ornamental leaf design

---
+++ resize-icons.ps1
@@ -0,0 +1,25 @@
+Add-Type -AssemblyName System.Drawing
+
+$src = "$PSScriptRoot\app-icon.png"
+$source = [System.Drawing.Image]::FromFile($src)
+Write-Host "Source: $($source.Width) x $($source.Height)"
+
+function Resize-Png([string]$outPath, [int]$size) {
+    $bmp = New-Object System.Drawing.Bitmap $size, $size
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $g.SmoothingMode      = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+    $g.InterpolationMode  = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $g.PixelOffsetMode    = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
+    $g.CompositingQuality = [System.Drawing.Drawing2D.CompositingQuality]::HighQuality
+    $g.DrawImage($source, 0, 0, $size, $size)
+    $bmp.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    $g.Dispose(); $bmp.Dispose()
+    Write-Host "Wrote $outPath ($size px)"
+}
+
+Resize-Png "$PSScriptRoot\icon-512.png"          512
+Resize-Png "$PSScriptRoot\icon-192.png"          192
+Resize-Png "$PSScriptRoot\apple-touch-icon.png"  180
+Resize-Png "$PSScriptRoot\favicon-32.png"         32
+
+$source.Dispose()
